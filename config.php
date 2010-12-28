b30255ed6c2f12fd73c47d9023191637acd85303
shunte <shunte@833bae32-548c-41c3-9e1f-c5543e30502e>
2010-12-28 23:52:47 +0000

    Configuration re-arch, intro info.php usage
    git-svn-id: https://svn.wdlxtv.com/svn/UMSP/plugins/dreambox-e2@91 833bae32-548c-41c3-9e1f-c5543e30502e

--- config.php
+++ config.php
@@ -1,28 +1,8 @@
 <?php
 
-# meta-name="Dreambox E2"
-# meta-desc="Dreambox E2"
-# meta-author="Toni"
-# meta-date="2010-05-03"
-# meta-version="0.2"
-# meta-type="umsp"
-# meta-url="http://forum.wdlxtv.com/viewtopic.php?f=53&t=320"
-# meta-filename="dreambox-e2.php"
-# meta-id="dreambox-e2"
-
-$pluginInfo = array (
-	'name'		=> 'Dreambox E2',
-	'desc'		=> 'Dreambox E2',
-	'author'	=> 'toni',
-	'date'		=> '2010-05-03',
-	'version'	=> '0.2',
-	'url'		=> 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=320',
-	'id'		=> 'dreambox-e2',
-	'art'		=> 'http://lh3.ggpht.com/_xJcSFBlLg_Y/TRLW3H10jUI/AAAAAAAAAHw/ZfICTGOW6q0/dreambox-e2.png',
-);
+	include('info.php');
 
 # _DONT_RUN_CONFIG_ gets set by external scripts that just want to get the pluginInfo array via include() without running any code. Better solution?
-
 if ( !defined('_DONT_RUN_CONFIG_') ) 
 {
 
