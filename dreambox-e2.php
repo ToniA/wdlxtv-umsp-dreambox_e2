3bac55b65a5dc58dfa4140073f08b388cf59bb61
shunte <shunte@833bae32-548c-41c3-9e1f-c5543e30502e>
2011-01-12 23:04:06 +0000

    Updates for dreambox-e2 plogin from Toni, overlaid files as psted - no additional functional or logical changes ro code base
    git-svn-id: https://svn.wdlxtv.com/svn/UMSP/plugins/dreambox-e2@113 833bae32-548c-41c3-9e1f-c5543e30502e

--- dreambox-e2.php
+++ dreambox-e2.php
@@ -1,50 +1,281 @@
 <?php
-	/*
+    // Dreambox E2 UMSP plugin by Toni
+    // http://forum.wdlxtv.com/viewtopic.php?f=49&t=320
 
-	Dreambox UMSP E2 plugin by Toni
-	http://forum.wdlxtv.com/viewtopic.php?f=49&t=320
+    function _pluginMain($prmQuery) {
 
-	Wrapper provided by shunte to conform to latest WEC based configuration
+      $queryData = array();
+      parse_str($prmQuery, $queryData);
 
-	*/
+      if ($queryData['bouquet'] != '') {
+        return Dreambox_E2_Channels($queryData);
+      }
+      elseif ($queryData['recordingspath'] != '') {
+        return Dreambox_E2_Recordings($queryData);
+      } else {
+          $myMediaItems[] = array(
+                'id'            => 'umsp://plugins/dreambox-e2?bouquet=root',
+                'parentID'      => 'umsp://plugins/dreambox-e2',
+                'restricted'    => '1',
+                'dc:title'      => 'Dreambox E2 Channels',
+                'upnp:class'    => 'object.container',
+                'upnp:album_art'=> '',
+        );
+        $myMediaItems[] = array(
+                'id'            => 'umsp://plugins/dreambox-e2?recordingspath=root',
+                'parentID'      => 'umsp://plugins/dreambox-e2',
+                'restricted'    => '1',
+                'dc:title'      => 'Dreambox E2 Recordings',
+                'upnp:class'    => 'object.container',
+                'upnp:album_art'=> '',
+        );
+       
+        return $myMediaItems;
+      }
+    }
 
-	function _pluginMain($prmQuery) {
+    function Dreambox_E2_Recordings($queryData) {
 
-		# If the DREAMBOX_HOSTNAME is not defined, the whole
-		# plugin is not visible in the menu
-   
-		if (file_exists('/conf/config')) 
-		{
-			$config = file_get_contents('/conf/config');         
-    			if(preg_match('/DREAMBOX_HOSTNAME=\'(.+)\'/', $config, $m)) 
-			{
-				$dreamboxAddress = $m[1];
-    			}
-		}
- 
-		if(isset($dreamoxAddress)&&($dreamoxAddress!=''))
-		{
+      if (file_exists('/conf/config')) {
+        $config = file_get_contents('/conf/config');
+       
+        if(preg_match('/DREAMBOX_HOSTNAME=\'(.+)\'/', $config, $results)) {
+          $dreamboxAddress = $results[1];
+        }
+      }
 
-			$myMediaItems[] = array(
-				'id'		=> 'umsp://plugins//dreambox-e2/dreambox-channels',
-				'parentID'	=> 'umsp://plugins/dreambox-e2',
-				'restricted'	=> '1',
-				'dc:title'	=> 'Dreambox E2 Channels',
-				'upnp:class'	=> 'object.container',
-				'upnp:album_art'=> '',
-			);
-			$myMediaItems[] = array(
-				'id'		=> 'umsp://plugins//dreambox-e2/dreambox-recordings',
-				'parentID'	=> 'umsp://plugins/dreambox-e2',
-				'restricted'	=> '1',
-				'dc:title'	=> 'Dreambox E2 Recordings',
-				'upnp:class'	=> 'object.container',
-				'upnp:album_art'=> '',
-			);
+      if ($queryData['recordingspath'] != 'root') {
+        $path = urldecode($queryData['recordingspath']);
+      } else {
+         # read the locations from Dreambox
+         # I assume there is only one location, in my case it's '/hdd/movie/'
 
-			return $myMediaItems;
+        # Location URL, list all recordings
+        $dreamboxLocationsUrl = 'web/getlocations';
 
-		}
-	} # end function
+        $reader = new XMLReader();
+        $locationsXML = file_get_contents('http://' . $dreamboxAddress . '/' . $dreamboxLocationsUrl);
+
+        $reader->XML($locationsXML);
+        while ($reader->read()) {
+          if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2locations')) {
+            #
+            # Read e2locations child nodes until end
+            #
+            do {
+                $reader->read();
+                if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2location')) {
+                  $path  = $reader->readString();
+                }
+            } while (!(($reader->nodeType == XMLReader::END_ELEMENT) && ($reader->localName == 'e2location')));
+          }
+        }
+      }
+
+    # First, read the media player items, as this is the *only* way to read the subdirectories
+
+      # Media URL, list media items under the given path
+      $dreamboxMedialistUrl = 'web/mediaplayerlist?path=' . $path;
+
+      $reader = new XMLReader();
+      $medialistXML = file_get_contents('http://' . $dreamboxAddress . '/' . $dreamboxMedialistUrl);
+
+      $reader->XML($medialistXML);
+      while ($reader->read()) {
+        if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2file')) {
+          #
+          # Read e2file child nodes until end
+          #
+          do {
+              $reader->read();
+              if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2isdirectory')) {
+                $newDir['isdirectory']  = $reader->readString();
+              }
+              if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2servicereference')) {
+                $newDir['directory'] = utf8_encode(utf8_decode($reader->readString()));
+              }
+          } while (!(($reader->nodeType == XMLReader::END_ELEMENT) && ($reader->localName == 'e2file')));
+          #
+          # New channelinfo item parsed. Now add as media item, unless it's the trashcan directory
+          # or the parent directory (WD TV remote has a 'back' button)
+          #
+       
+          if ( $newDir['isdirectory'] == 'True' &&
+               strncmp($path, $newDir['directory'], strlen($newDir['directory'])) &&
+               $newDir['directory'] != $path . '.trashcan/' ) {         
+            $retMediaItems[] = array (
+              'id'        => 'umsp://plugins/dreambox-e2?recordingspath=' . urlencode($newDir['directory']),
+              'dc:title'  => utf8_encode(utf8_decode(basename($newDir['directory']))),
+              'upnp:class' => 'object.container'
+            );
+          }
+        } # end if
+      } #end while
+
+
+    # Now, read the movie information from this directory
+
+      # Movies URL, list all recordings
+      $dreamboxMovielistUrl = 'web/movielist?dirname=' . $path;
+
+      #$reader = new XMLReader();
+      $movielistXML = file_get_contents('http://' . $dreamboxAddress . '/' . $dreamboxMovielistUrl);
+
+      $reader->XML($movielistXML);
+      while ($reader->read()) {
+        if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2movie')) {
+          #
+          # Read channelinfo child nodes until end
+          #
+          do {
+              $reader->read();
+                  if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2filename')) {
+              $newMovie['id']  = $reader->readString();
+            }
+                  if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2title')) {
+              $newMovie['title'] = utf8_encode(utf8_decode($reader->readString()));
+                  }
+                  if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2description')) {
+              $newMovie['description'] = utf8_encode(utf8_decode($reader->readString()));
+                  }
+                  if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2filesize')) {
+              $newMovie['filesize'] = $reader->readString();
+                  }
+          } while (!(($reader->nodeType == XMLReader::END_ELEMENT) && ($reader->localName == 'e2movie')));
+          #
+          # New channelinfo item parsed. Now add as media item:
+          #
+       
+          $retMediaItems[] = array (
+            'id'        => $newMovie['id'],
+            'res'       => 'http://' . $dreamboxAddress . ':80/file?file=' . $newMovie['id'],
+    #        'res'       => 'http://localhost/umsp/plugins/dreambox-e2/dreambox-proxy.php?itemUrl=http://' . $dreamboxAddress . ':80/file?file=' . $newMovie['id'],
+            'dc:title'  => $newMovie['title'],
+            'desc'      => $newMovie['description'],
+            'size'      => $newMovie['filesize'],
+            'upnp:class'    => 'object.item.videoitem',
+            'protocolInfo'    => 'http-get:*:video/mpeg:*'
+          );
+        } # end if
+      } #end while
+
+      return $retMediaItems;
+    } # end function
+
+
+    function Dreambox_E2_Channels($queryData) {
+
+      # If the DREAMBOX_HOSTNAME is not defined, the whole
+      # plugin is not visible in the menu
+       
+      if (file_exists('/conf/config')) {
+        $config = file_get_contents('/conf/config');
+             
+        if(preg_match('/DREAMBOX_HOSTNAME=\'(.+)\'/', $config, $results)) {
+          $dreamboxAddress = $results[1];
+        }
+      } else {
+         return; 
+      }
+
+      # Do we have a bouquet as the parameter?
+
+      if ($queryData['bouquet'] != 'root') {
+        $bouquet = $queryData['bouquet'];
+      } else {
+        $bouquet = '';   
+      }
+
+      # No, we need to ask for the list of bouquets
+      if ( $bouquet == '' ) {
+
+        # Get all bouquets
+        $dreamboxBouquetsUrl = 'web/getservices';
+
+        $reader = new XMLReader();
+        $bouquetsXML = file_get_contents('http://' . $dreamboxAddress . '/' . $dreamboxBouquetsUrl);
+
+        $reader->XML($bouquetsXML);
+        while ($reader->read()) {
+          if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2service')) {
+            #
+            # Read e2service child nodes until end
+            #
+
+            do {
+                $reader->read();
+                if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2servicereference')) {
+                  $newBouquet['sref']  = $reader->readString();
+                }
+                if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2servicename')) {
+                  $newBouquet['title'] = utf8_decode($reader->readString());
+                }
+            } while (!(($reader->nodeType == XMLReader::END_ELEMENT) && ($reader->localName == 'e2service')));
+            #
+            # New bouquet item parsed. Now add as media item:
+            #
+             
+            $retMediaItems[] = array (
+              'id'         => 'umsp://plugins/dreambox-e2/dreambox-channels?bouquet=' . $newBouquet['sref'],
+              'dc:title'   => utf8_encode(utf8_decode($newBouquet['title'])),
+              'upnp:class' => 'object.container'
+            );   
+          } # end if
+        } #end while
+
+        # If there's just one bouquet, jump directly into the bouquet contents
+        # Othervise show the bouquet list
+
+        if ( count($retMediaItems) == 1 ) {   
+          return _pluginMain('bouquet=' . $newBouquet['sref']);
+        } else {
+          return $retMediaItems;     
+        }
+      } else {
+        # We have a bouquet sRef as a parameter -> list all channels in that bouquet
+
+        # $bouquet looks like '1:7:1:0:0:0:0:0:0:0:FROM BOUQUET \"userbouquet.favourites.tv\" ORDER BY bouquet',
+        # but we should feed it in as '1:7:1:0:0:0:0:0:0:0:FROM%20BOUQUET%20%22userbouquet.favourites.tv%22%20ORDER%20BY%20bouquet'
+
+        $dreamboxServiceUrl = 'web/getservices?sRef=' . str_replace('\\"', "%22", str_replace(" ", "%20", $bouquet));
+       
+        $reader = new XMLReader();
+        $channellistXML = file_get_contents('http://' . $dreamboxAddress . '/' . $dreamboxServiceUrl);
+
+        $reader->XML($channellistXML);
+        while ($reader->read()) {
+          if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2service')) {
+            #
+            # Read channelinfo child nodes until end
+            #
+            do {
+              $reader->read();
+              if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2servicereference')) {
+                $newChannel['id']  = $reader->readString();
+              }
+              if (($reader->nodeType == XMLReader::ELEMENT) && ($reader->localName == 'e2servicename')) {
+                $newChannel['title'] = $reader->readString();
+              }
+            } while (!(($reader->nodeType == XMLReader::END_ELEMENT) && ($reader->localName == 'e2service')));
+            #
+            # New channelinfo item parsed. Now add as media item:
+            #
+
+            $retMediaItems[] = array (
+              'id'             => $newChannel['id'],
+    # Requires my custom streamproxy binary on the Dreambox
+    #          'res'            => 'http://' . $dreamboxAddress . ':8001/' . $newChannel['id'],
+              'res'            => 'http://localhost/umsp/plugins/dreambox-e2/dreambox-proxy.php?itemUrl=http://' . $dreamboxAddress . ':8001/' . $newChannel['id'],
+              'dc:title'       => utf8_encode(utf8_decode($newChannel['title'])),
+              'upnp:class'     => 'object.item.videoitem',
+              # picons should be under /usr/lib/enigma2/python/Plugins/Extensions/WebInterface/web-data/streampage on the Dreambox
+              'upnp:album_art' => 'http://' . $dreamboxAddress . '/web-data/streampage/' . rawurlencode($newChannel['title']) . '.png',
+              'protocolInfo'   => '*:*:*:*'
+            );
+          } # end if
+        } # end while
+        return $retMediaItems;
+      } # end if
+    }
 ?>
 
--- dreambox-proxy.php
+++ dreambox-proxy.php
@@ -1,77 +1,75 @@
 <?php
-// Dreambox E2 plugin by Toni
-// http://forum.wdlxtv.com/viewtopic.php?f=49&t=320
 
-# Immediately respond to 'HEAD' requests
-if ( $_SERVER[''] == 'HEAD' ) {
-   header('Content-Type: video/mpeg');
-   exit;
-}
+    # Immediately respond to 'HEAD' requests
+    if ( $_SERVER[''] == 'HEAD' ) {
+       header('Content-Type: video/mpeg');
+       exit;
+    }
 
-# itemUrl is like
-# itemUrl=http://dm7025:8001/1:0:1:11:1001:20F6:EEEE0000:0:0:0:
+    # itemUrl is like
+    # itemUrl=http://dm7025:8001/1:0:1:11:1001:20F6:EEEE0000:0:0:0:
 
-$rawURL = $_GET['itemUrl'];
-$parsedURL = parse_url($rawURL);
+    $rawURL = $_GET['itemUrl'];
+    $parsedURL = parse_url($rawURL);
 
-$itemHost = $parsedURL['host'];
-$itemPort = $parsedURL['port'];
-$itemPath = $parsedURL['path'];
-$itemQuery = $parsedURL['query'];
+    $itemHost = $parsedURL['host'];
+    $itemPort = $parsedURL['port'];
+    $itemPath = $parsedURL['path'];
+    $itemQuery = $parsedURL['query'];
 
-# Only one stream at a time, so refuse to connect if there's already an established connection
-#system('netstat -a | grep ' . $itemHost . ' | grep ESTABLISHED', $retval);
+    # Only one stream at a time, so refuse to connect if there's already an established connection
+    #system('netstat -a | grep ' . $itemHost . ' | grep ESTABLISHED', $retval);
 
-#if ( ! $retval ) {
-#   header('Content-Type: video/mpeg');
-#   exit;
-#kkkk}
+    #if ( ! $retval ) {
+    #   header('Content-Type: video/mpeg');
+    #   exit;
+    #kkkk}
 
-_dreamboxGet($itemHost, $itemPort, $itemPath);
+    _dreamboxGet($itemHost, $itemPort, $itemPath);
 
-function _dreamboxGet($prmHost, $prmPort, $prmPath) {
-   $fp = fsockopen($prmHost, $prmPort, $errno, $errstr);
-   if (!$fp) {
-      echo "$errstr ($errno)<br />\n";
-   } else {
-      # Create the HTTP GET request
-      
-      $out  = "GET $prmPath HTTP/1.0\r\n";
-      $out .= "User-Agent: Wget/1.12\r\n";
-      $out .= "Accept: */*\r\n";
-      $out .= "Host: $prmHost:$prmPort\r\n";
-      $out .= "Connection: Keep-Alive\r\n";
-      $out .= "\r\n";
-   
-      fwrite($fp, $out);
-      
-      # Create HTTP headers for WDTV
-      # Streaming starts much faster with these
-      # 'Content-Size' and 'Content-Length' headers
-      
-      header("Content-Type: video/mpeg");
-      header("Content-Size: 65535");
-      header("Content-Length: 65535");
-      
-      # Ignore the original headers
-      
-      $headerpassed = false;
-      while ($headerpassed == false) {
-         $line = fgets($fp);
-         if ( $line == "\r\n" ) {
-            $headerpassed = true;
-         }
-      }
+    function _dreamboxGet($prmHost, $prmPort, $prmPath) {
+       $fp = fsockopen($prmHost, $prmPort, $errno, $errstr);
+       if (!$fp) {
+          echo "$errstr ($errno)<br />\n";
+       } else {
+          # Create the HTTP GET request
          
-      # Pass thru the DVB transport stream
-      # It's important to disable the time limit
-      
-      set_time_limit(0);
-      fpassthru($fp);
-      set_time_limit(30);
-      
-      fclose($fp);
-   }
-}
+          $out  = "GET $prmPath HTTP/1.0\r\n";
+          $out .= "User-Agent: Wget/1.12\r\n";
+          $out .= "Accept: */*\r\n";
+          $out .= "Host: $prmHost:$prmPort\r\n";
+          $out .= "Connection: Keep-Alive\r\n";
+          $out .= "\r\n";
+       
+          fwrite($fp, $out);
+         
+          # Create HTTP headers for WDTV
+          # Streaming starts much faster with these
+          # 'Content-Size' and 'Content-Length' headers
+         
+          header("Content-Type: video/mpeg");
+          header("Content-Size: 65535");
+          header("Content-Length: 65535");
+         
+          # Ignore the original headers
+         
+          $headerpassed = false;
+          while ($headerpassed == false) {
+             $line = fgets($fp);
+             if ( $line == "\r\n" ) {
+                $headerpassed = true;
+             }
+          }
+             
+          # Pass thru the DVB transport stream
+          # It's important to disable the time limit
+         
+          set_time_limit(0);
+          fpassthru($fp);
+          set_time_limit(30);
+         
+          fclose($fp);
+       }
+    }
 ?>
 
