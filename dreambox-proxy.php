e91efc67b25bb84c97e6da519f66a3f1fee2aa36
Toni <Toni@833bae32-548c-41c3-9e1f-c5543e30502e>
2011-01-29 09:32:14 +0000

    Working live TV streaming
    git-svn-id: https://svn.wdlxtv.com/svn/UMSP/plugins/dreambox-e2@123 833bae32-548c-41c3-9e1f-c5543e30502e

--- dreambox-proxy.php
+++ dreambox-proxy.php
@@ -1,75 +1,74 @@
 <?php
 
-    # Immediately respond to 'HEAD' requests
-    if ( $_SERVER[''] == 'HEAD' ) {
-       header('Content-Type: video/mpeg');
-       exit;
+# itemUrl is like
+# itemUrl=http://dm7025:8001/1:0:1:11:1001:20F6:EEEE0000:0:0:0:
+
+$rawURL = $_GET['itemUrl'];
+$parsedURL = parse_url($rawURL);
+
+# Remove the resume point from the /tmp/video_resume_point_table file
+# It's of no use to try to resume a live stream...
+
+#exec('perl -0135 -i.bak -ne "print unless /$rawURL/;" /tmp/video_resume_point_table');
+
+error_log('URL: ' . $rawURL);
+
+# Immediately respond to 'HEAD' requests
+if ( $_SERVER['REQUEST_METHOD'] == 'HEAD' ) {
+  header('HTTP/1.0 200 OK');
+  header('Connection: Close');
+  header('ContentFeatures.DLNA.ORG: DLNA.ORG_OP=00;DLNA.ORG_CI=1');
+  header('Content-Type: video/mpeg');
+  header('Server: stream_enigma2');
+
+  return;
+}
+
+$itemHost = $parsedURL['host'];
+$itemPort = $parsedURL['port'];
+$itemPath = $parsedURL['path'];
+
+_dreamboxGet($itemHost, $itemPort, $itemPath);
+
+    
+function _dreamboxGet($prmHost, $prmPort, $prmPath) {
+  $fp = fsockopen($prmHost, $prmPort, $errno, $errstr);
+  if (!$fp) {
+    echo "$errstr ($errno)<br />\n";
+  } else {
+    # Create the HTTP GET request for Dreambox
+    
+    $out  = "GET $prmPath HTTP/1.0\r\n";
+    $out .= "User-Agent: Wget/1.12\r\n";
+    $out .= "Accept: */*\r\n";
+    $out .= "Host: $prmHost:$prmPort\r\n";
+    $out .= "Connection: Keep-Alive\r\n";
+    $out .= "\r\n";
+    
+    fwrite($fp, $out);
+    
+    # Add the ContentFeatures.DLNA.ORG to the original headers
+    
+    $headerpassed = false;
+    while ($headerpassed == false) {
+      $line = fgets($fp);
+                   
+      if ( $line == "\r\n" ) {
+        header('ContentFeatures.DLNA.ORG: DLNA.ORG_OP=00;DLNA.ORG_CI=1'); 
+        $headerpassed = true;
+      } else {
+        header($line);
+      }
     }
-
-    # itemUrl is like
-    # itemUrl=http://dm7025:8001/1:0:1:11:1001:20F6:EEEE0000:0:0:0:
-
-    $rawURL = $_GET['itemUrl'];
-    $parsedURL = parse_url($rawURL);
-
-    $itemHost = $parsedURL['host'];
-    $itemPort = $parsedURL['port'];
-    $itemPath = $parsedURL['path'];
-    $itemQuery = $parsedURL['query'];
-
-    # Only one stream at a time, so refuse to connect if there's already an established connection
-    #system('netstat -a | grep ' . $itemHost . ' | grep ESTABLISHED', $retval);
-
-    #if ( ! $retval ) {
-    #   header('Content-Type: video/mpeg');
-    #   exit;
-    #kkkk}
-
-    _dreamboxGet($itemHost, $itemPort, $itemPath);
-
-    function _dreamboxGet($prmHost, $prmPort, $prmPath) {
-       $fp = fsockopen($prmHost, $prmPort, $errno, $errstr);
-       if (!$fp) {
-          echo "$errstr ($errno)<br />\n";
-       } else {
-          # Create the HTTP GET request
-         
-          $out  = "GET $prmPath HTTP/1.0\r\n";
-          $out .= "User-Agent: Wget/1.12\r\n";
-          $out .= "Accept: */*\r\n";
-          $out .= "Host: $prmHost:$prmPort\r\n";
-          $out .= "Connection: Keep-Alive\r\n";
-          $out .= "\r\n";
-       
-          fwrite($fp, $out);
-         
-          # Create HTTP headers for WDTV
-          # Streaming starts much faster with these
-          # 'Content-Size' and 'Content-Length' headers
-         
-          header("Content-Type: video/mpeg");
-          header("Content-Size: 65535");
-          header("Content-Length: 65535");
-         
-          # Ignore the original headers
-         
-          $headerpassed = false;
-          while ($headerpassed == false) {
-             $line = fgets($fp);
-             if ( $line == "\r\n" ) {
-                $headerpassed = true;
-             }
-          }
-             
-          # Pass thru the DVB transport stream
-          # It's important to disable the time limit
-         
-          set_time_limit(0);
-          fpassthru($fp);
-          set_time_limit(30);
-         
-          fclose($fp);
-       }
-    }
+    
+    # Pass thru the DVB transport stream
+    # It's important to disable the time limit
+    
+    set_time_limit(0);
+    fpassthru($fp);
+    set_time_limit(30);
+    
+    fclose($fp);
+  }
+}
 ?>
-
