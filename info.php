9f077e54a68c18153933345ebda215dc52abbc45
shunte <shunte@833bae32-548c-41c3-9e1f-c5543e30502e>
2011-05-07 17:14:21 +0000

    update of badge art for dreambox, eyetv, twitv and rlsnet - meta tags and info attributes no additional code changes
    git-svn-id: https://svn.wdlxtv.com/svn/UMSP/plugins/dreambox-e2@205 833bae32-548c-41c3-9e1f-c5543e30502e

--- info.php
+++ info.php
@@ -9,7 +9,7 @@
 # meta-filename="dreambox-e2.php"
 # meta-id="dreambox-e2"
 # meta-basefirmware="*"
-# meta-thumbnail="http://lh3.ggpht.com/_xJcSFBlLg_Y/TRLW3H10jUI/AAAAAAAAAHw/ZfICTGOW6q0/s200/dreambox-e2.png"
+# meta-thumbnail="http://lh3.googleusercontent.com/_xJcSFBlLg_Y/TcVCQBFT17I/AAAAAAAABCw/hRBdYG86VGw/s200/dreambox-planar-contrast.png"
 # meta-rating="General"
 # meta-category="Entertainment"
 
@@ -22,8 +22,8 @@
 		'version'	=> '0.2.1',
 		'url'		=> 'http://forum.wdlxtv.com/viewtopic.php?f=53&t=320',
 		'id'		=> 'dreambox-e2',
-		'thumb'		=> 'http://lh3.ggpht.com/_xJcSFBlLg_Y/TRLW3H10jUI/AAAAAAAAAHw/ZfICTGOW6q0/s60/dreambox-e2.png',
-		'art'		=> 'http://lh3.ggpht.com/_xJcSFBlLg_Y/TRLW3H10jUI/AAAAAAAAAHw/ZfICTGOW6q0/s200/dreambox-e2.png',
+		'thumb'		=> 'http://lh3.googleusercontent.com/_xJcSFBlLg_Y/TcVCQBFT17I/AAAAAAAABCw/hRBdYG86VGw/s60/dreambox-planar-contrast.png',
+		'art'		=> 'http://lh3.googleusercontent.com/_xJcSFBlLg_Y/TcVCQBFT17I/AAAAAAAABCw/hRBdYG86VGw/s200/dreambox-planar-contrast.png',
 	);
 
 ?>
